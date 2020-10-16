72092e44e25156aa385fca5fa0bef8a837c71247
Alex Chung <adchung@ncsu.edu>
2020-10-16 02:03:15 -0400

    Updated build scripts for backend (Need further tuning)

---
+++ build/lib/backend-stack.ts
@@ -0,0 +1,31 @@
+import * as cdk from "@aws-cdk/core";
+import * as dynamodb from "@aws-cdk/aws-dynamodb"
+import * as lambda from "@aws-cdk/aws-lambda"
+import * as apigateway from "@aws-cdk/aws-apigateway";
+
+export class BackendStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const locationsDatabaseTable = new dynamodb.Table(this, "Locations",{
+            partitionKey: { name: "name", type: dynamodb.AttributeType.STRING },
+        });
+
+        const locationsApiEndpointHandler = new lambda.Function(this, "LocationsAPIEndpointHandler", {
+            runtime: lambda.Runtime.GO_1_X,
+            code: lambda.Code.fromAsset("functions"),
+            handler: "function.handler",
+        });
+
+        //Let the lambda API have RW access to the db
+        locationsDatabaseTable.grantReadWriteData(locationsApiEndpointHandler);
+
+        const api = new apigateway.RestApi(this, "LocationsAPI");
+        const locations = api.root.resourceForPath("/locations");
+        locations.addMethod("GET", new apigateway.LambdaIntegration(locationsApiEndpointHandler));
+        locations.addMethod("POST", new apigateway.LambdaIntegration(locationsApiEndpointHandler));
+        locations.addMethod("PUT", new apigateway.LambdaIntegration(locationsApiEndpointHandler));
+        locations.addMethod("DELETE", new apigateway.LambdaIntegration(locationsApiEndpointHandler));
+    }
+}
+
